6a9816acab5852f08bd6a0b7283802651b0f09de
aluno <Aluno@PR02-302-16.CSED.LAB>
2023-08-23 11:36:39 -0300

    Aula de mudança de activities

---
+++ AppNavActivities/app/src/main/java/com/example/appnavactivities/TerceiraActivity.kt
@@ -0,0 +1,11 @@
+package com.example.appnavactivities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TerceiraActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_terceira)
+    }
+}
